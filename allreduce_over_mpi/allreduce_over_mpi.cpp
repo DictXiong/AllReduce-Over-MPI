ca9cf60a1160f322183805dd736a85348e83676f
Dict Xiong <me@beardic.cn>
2021-05-15 15:19:21 +0800

    reduce_sum and openmp enabled

--- allreduce_over_mpi/allreduce_over_mpi.cpp
+++ allreduce_over_mpi/allreduce_over_mpi.cpp
@@ -132,6 +132,211 @@ public:
     }
 };
 
+template<typename T>
+void reduce_sum(T **src, int num_blocks, size_t num_elements)
+{
+#define PARALLEL_THREAD 14
+    T *dst = src[0];
+    T *src0 = src[0];
+    T *src1 = src[1];
+    T *src2 = src[2];
+    T *src3 = src[3];
+    T *src4 = src[4];
+    T *src5 = src[5];
+    T *src6 = src[6];
+    T *src7 = src[7];
+    T *src8 = src[8];
+    T *src9 = src[9];
+    T *src10 = src[10];
+    T *src11 = src[11];
+    T *src12 = src[12];
+    T *src13 = src[13];
+    T *src14 = src[14];
+    T *src15 = src[15];
+    T *src16 = src[16];
+    T *src17 = src[17];
+    T *src18 = src[18];
+    T *src19 = src[19];
+
+    switch (num_blocks)
+    {
+    case 2:
+    {
+#pragma omp parallel for simd num_threads(PARALLEL_THREAD)
+        for (size_t i = 0; i < num_elements; ++i)
+        {
+            dst[i] = src0[i] + src1[i];
+        }
+        break;
+    }
+    case 3:
+    {
+#pragma omp parallel for simd num_threads(PARALLEL_THREAD)
+        for (size_t i = 0; i < num_elements; ++i)
+        {
+            dst[i] = src0[i] + src1[i] + src2[i];
+        }
+        break;
+    }
+    case 4:
+    {
+#pragma omp parallel for simd num_threads(PARALLEL_THREAD)
+        for (size_t i = 0; i < num_elements; ++i)
+        {
+            dst[i] = src0[i] + src1[i] + src2[i] + src3[i];
+        }
+        break;
+    }
+    case 5:
+    {
+#pragma omp parallel for simd num_threads(PARALLEL_THREAD)
+        for (size_t i = 0; i < num_elements; ++i)
+        {
+            dst[i] = src0[i] + src1[i] + src2[i] + src3[i] + src4[i];
+        }
+        break;
+    }
+    case 6:
+    {
+#pragma omp parallel for simd num_threads(PARALLEL_THREAD)
+        for (size_t i = 0; i < num_elements; ++i)
+        {
+            dst[i] = src0[i] + src1[i] + src2[i] + src3[i] + src4[i] + src5[i];
+        }
+        break;
+    }
+    case 7:
+    {
+#pragma omp parallel for simd num_threads(PARALLEL_THREAD)
+        for (size_t i = 0; i < num_elements; ++i)
+        {
+            dst[i] = src0[i] + src1[i] + src2[i] + src3[i] + src4[i] + src5[i] + src6[i];
+        }
+        break;
+    }
+    case 8:
+    {
+#pragma omp parallel for simd num_threads(PARALLEL_THREAD)
+        for (size_t i = 0; i < num_elements; ++i)
+        {
+            dst[i] = src0[i] + src1[i] + src2[i] + src3[i] + src4[i] + src5[i] + src6[i] + src7[i];
+        }
+        break;
+    }
+    case 9:
+    {
+#pragma omp parallel for simd num_threads(PARALLEL_THREAD)
+        for (size_t i = 0; i < num_elements; ++i)
+        {
+            dst[i] = src0[i] + src1[i] + src2[i] + src3[i] + src4[i] + src5[i] + src6[i] + src7[i] + src8[i];
+        }
+        break;
+    }
+    case 10:
+    {
+#pragma omp parallel for simd num_threads(PARALLEL_THREAD)
+        for (size_t i = 0; i < num_elements; ++i)
+        {
+            dst[i] = src0[i] + src1[i] + src2[i] + src3[i] + src4[i] + src5[i] + src6[i] + src7[i] + src8[i] + src9[i];
+        }
+        break;
+    }
+    case 11:
+    {
+#pragma omp parallel for simd num_threads(PARALLEL_THREAD)
+        for (size_t i = 0; i < num_elements; ++i)
+        {
+            dst[i] = src0[i] + src1[i] + src2[i] + src3[i] + src4[i] + src5[i] + src6[i] + src7[i] + src8[i] + src9[i] + src10[i];
+        }
+        break;
+    }
+    case 12:
+    {
+#pragma omp parallel for simd num_threads(PARALLEL_THREAD)
+        for (size_t i = 0; i < num_elements; ++i)
+        {
+            dst[i] = src0[i] + src1[i] + src2[i] + src3[i] + src4[i] + src5[i] + src6[i] + src7[i] + src8[i] + src9[i] + src10[i] + src11[i];
+        }
+        break;
+    }
+    case 13:
+    {
+#pragma omp parallel for simd num_threads(PARALLEL_THREAD)
+        for (size_t i = 0; i < num_elements; ++i)
+        {
+            dst[i] = src0[i] + src1[i] + src2[i] + src3[i] + src4[i] + src5[i] + src6[i] + src7[i] + src8[i] + src9[i] + src10[i] + src11[i] + src12[i];
+        }
+        break;
+    }
+    case 14:
+    {
+#pragma omp parallel for simd num_threads(PARALLEL_THREAD)
+        for (size_t i = 0; i < num_elements; ++i)
+        {
+            dst[i] = src0[i] + src1[i] + src2[i] + src3[i] + src4[i] + src5[i] + src6[i] + src7[i] + src8[i] + src9[i] + src10[i] + src11[i] + src12[i] + src13[i];
+        }
+        break;
+    }
+    case 15:
+    {
+#pragma omp parallel for simd num_threads(PARALLEL_THREAD)
+        for (size_t i = 0; i < num_elements; ++i)
+        {
+            dst[i] = src0[i] + src1[i] + src2[i] + src3[i] + src4[i] + src5[i] + src6[i] + src7[i] + src8[i] + src9[i] + src10[i] + src11[i] + src12[i] + src13[i] + src14[i];
+        }
+        break;
+    }
+    case 16:
+    {
+#pragma omp parallel for simd num_threads(PARALLEL_THREAD)
+        for (size_t i = 0; i < num_elements; ++i)
+        {
+            dst[i] = src0[i] + src1[i] + src2[i] + src3[i] + src4[i] + src5[i] + src6[i] + src7[i] + src8[i] + src9[i] + src10[i] + src11[i] + src12[i] + src13[i] + src14[i] + src15[i];
+        }
+        break;
+    }
+    case 17:
+    {
+#pragma omp parallel for simd num_threads(PARALLEL_THREAD)
+        for (size_t i = 0; i < num_elements; ++i)
+        {
+            dst[i] = src0[i] + src1[i] + src2[i] + src3[i] + src4[i] + src5[i] + src6[i] + src7[i] + src8[i] + src9[i] + src10[i] + src11[i] + src12[i] + src13[i] + src14[i] + src15[i] + src16[i];
+        }
+        break;
+    }
+    case 18:
+    {
+#pragma omp parallel for simd num_threads(PARALLEL_THREAD)
+        for (size_t i = 0; i < num_elements; ++i)
+        {
+            dst[i] = src0[i] + src1[i] + src2[i] + src3[i] + src4[i] + src5[i] + src6[i] + src7[i] + src8[i] + src9[i] + src10[i] + src11[i] + src12[i] + src13[i] + src14[i] + src15[i] + src16[i] + src17[i];
+        }
+        break;
+    }
+    case 19:
+    {
+#pragma omp parallel for simd num_threads(PARALLEL_THREAD)
+        for (size_t i = 0; i < num_elements; ++i)
+        {
+            dst[i] = src0[i] + src1[i] + src2[i] + src3[i] + src4[i] + src5[i] + src6[i] + src7[i] + src8[i] + src9[i] + src10[i] + src11[i] + src12[i] + src13[i] + src14[i] + src15[i] + src16[i] + src17[i] + src18[i];
+        }
+        break;
+    }
+    case 20:
+    {
+#pragma omp parallel for simd num_threads(PARALLEL_THREAD)
+        for (size_t i = 0; i < num_elements; ++i)
+        {
+            dst[i] = src0[i] + src1[i] + src2[i] + src3[i] + src4[i] + src5[i] + src6[i] + src7[i] + src8[i] + src9[i] + src10[i] + src11[i] + src12[i] + src13[i] + src14[i] + src15[i] + src16[i] + src17[i] + src18[i] + src19[i];
+        }
+        break;
+    }
+    default:
+        std::cerr << "Unknown num_blocks: " << num_blocks << std::endl;
+        break;
+    }
+}
+
 int main(int argc, char **argv)
 {
     FLAGS_colorlogtostderr = true;
